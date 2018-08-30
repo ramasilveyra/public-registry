81911cf563dd730a87217aab21db32a2758b0eac
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2018-08-29 22:11:47 -0300

    gitpkg

---
+++ build/ast.js
@@ -0,0 +1,105 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+// Root.
+const rootName = exports.rootName = 'Program';
+const createRoot = exports.createRoot = () => ({
+  type: rootName,
+  children: []
+});
+
+// HTML elements.
+const elementName = exports.elementName = 'Element';
+const createElement = exports.createElement = (tagName, attributes = []) => ({
+  type: elementName,
+  tagName,
+  attributes,
+  children: []
+});
+
+// HTML text elements.
+const textName = exports.textName = 'Text';
+const createText = exports.createText = value => ({
+  type: textName,
+  value
+});
+
+// HTML attributes.
+const attributeName = exports.attributeName = 'Attribute';
+const createAttribute = exports.createAttribute = ({
+  name,
+  value,
+  valuePath,
+  valueNode,
+  isBoolean = false,
+  isNode = false,
+  isString = false
+}) => ({
+  type: attributeName,
+  name,
+  value,
+  valuePath,
+  valueNode,
+  isBoolean,
+  isString,
+  isNode
+});
+
+// Template engine syntax for inline JavaScript.
+// export const scriptletName = 'Scriptlet';
+// export const createScriptlet = value => ({
+//   type: scriptletName,
+//   value
+// });
+
+// Template engine syntax for escaped interpolation.
+const interpolationEscapedName = exports.interpolationEscapedName = 'InterpolationEscaped';
+const createInterpolationEscaped = exports.createInterpolationEscaped = valuePath => ({
+  type: interpolationEscapedName,
+  valuePath
+});
+
+// Template engine syntax for unescaped interpolation.
+// export const interpolationUnescapedName = 'InterpolationUnescaped';
+// export const createInterpolationUnescaped = value => ({
+//   type: interpolationUnescapedName,
+//   value
+// });
+
+// Template engine syntax for condition.
+const conditionName = exports.conditionName = 'Condition';
+const createCondition = exports.createCondition = ({ testPath, consequent, alternate = null }) => ({
+  type: conditionName,
+  testPath,
+  consequent,
+  alternate
+});
+
+// Template engine syntax for iteration.
+const iterationName = exports.iterationName = 'Iteration';
+const createIteration = exports.createIteration = ({
+  iterablePath,
+  currentValuePath,
+  indexPath,
+  arrayPath,
+  body = null
+}) => ({
+  type: iterationName,
+  iterablePath,
+  currentValuePath,
+  indexPath,
+  arrayPath,
+  body
+});
+
+// Template engine syntax for mixin.
+const mixinName = exports.mixinName = 'Mixin';
+const createMixin = exports.createMixin = (name, props) => ({
+  type: mixinName,
+  name,
+  props,
+  children: []
+});
+//# sourceMappingURL=ast.js.map
