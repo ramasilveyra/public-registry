89c1526d7b536c3a6737ca6b30d4e20638cfd125
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2019-03-27 16:53:21 -0500

    gitpkg

---
+++ CHANGELOG.md
@@ -0,0 +1,77 @@
+# Nuka Changelog
+
+## 4.5.3 (2019-03-18)
+
+- Resolve dependency vulnerabilities
+- [#506](https://github.com/FormidableLabs/nuka-carousel/pull/506) Add Scroll3D transition mode option
+
+## 4.5.2 (2019-03-08)
+
+- Fixes dragging bug
+
+## 4.5.1 (2019-03-06)
+
+- [#511](https://github.com/FormidableLabs/nuka-carousel/pull/511) Adds style and transitionMode types
+- [#510](https://github.com/FormidableLabs/nuka-carousel/pull/510) Adds new prop to allow autoplay in reverse
+
+## 4.4.9 (2019-03-04)
+
+- [#508](https://github.com/FormidableLabs/nuka-carousel/pull/508) Fix speed slide flicker/blinking
+
+## 4.4.8 (2019-02-21)
+
+- [#501](https://github.com/FormidableLabs/nuka-carousel/pull/501) Add cellAlign type to CarouselSlideRenderControlProps interface
+- [#500](https://github.com/FormidableLabs/nuka-carousel/pull/500) Animation performance enhancements
+- [#496](https://github.com/FormidableLabs/nuka-carousel/pull/496) Upgrade react-move
+- [#492](https://github.com/FormidableLabs/nuka-carousel/pull/492) Fix Paging Dots sync, fix 'Next' button validation
+- [#490](https://github.com/FormidableLabs/nuka-carousel/pull/490) Disable animation for an initial slide when non-default slideIndex prop is passed
+- [#489](https://github.com/FormidableLabs/nuka-carousel/pull/489) Disable animation prop created
+- [#488](https://github.com/FormidableLabs/nuka-carousel/pull/488) Configurable zoom scale prop
+- [#487](https://github.com/FormidableLabs/nuka-carousel/pull/487) updates to README
+
+## 4.4.7 (2019-01-21)
+
+- [#480](https://github.com/FormidableLabs/nuka-carousel/pull/480) Add Types to build
+- [#486](https://github.com/FormidableLabs/nuka-carousel/pull/486) Fix mouse event handling to account for click events within the slide
+
+## 4.4.6 (2019-01-12)
+
+- [#477](https://github.com/FormidableLabs/nuka-carousel/pull/477) Add TypeScript types
+- [#475](https://github.com/FormidableLabs/nuka-carousel/pull/475) Fix click event handler on button within slide element
+- [#473](https://github.com/FormidableLabs/nuka-carousel/pull/473) Add zoom effect prop
+
+## 4.4.5 (2018-12-31)
+
+- [#469](https://github.com/FormidableLabs/nuka-carousel/pull/469) Fix initial height issue
+
+## 4.4.4 (2018-12-07)
+
+- [#460](https://github.com/FormidableLabs/nuka-carousel/pull/460) Ability to configure aria-live message with prop
+- [#458](https://github.com/FormidableLabs/nuka-carousel/pull/458) Fix flicker in wrap-around
+- [#453](https://github.com/FormidableLabs/nuka-carousel/pull/453) Fix issue involving updating props on beforeSlide
+
+## 4.4.3 (2018-11-16)
+
+- [#451](https://github.com/FormidableLabs/nuka-carousel/pull/451) Allow clicks with modifier present
+- [#446](https://github.com/FormidableLabs/nuka-carousel/pull/446) Fix for demo (toggle between 2 and 6 slides showing)
+- [#445](https://github.com/FormidableLabs/nuka-carousel/pull/445) Add `disableKeyboardControls` prop to allow users to disable keyboard controls
+- [#441](https://github.com/FormidableLabs/nuka-carousel/pull/441) Start work on removing deprecated lifecycle methods
+- [#439](https://github.com/FormidableLabs/nuka-carousel/pull/439) Allow `pauseOnHover` to work when swiping/dragging is disabled
+- [#436](https://github.com/FormidableLabs/nuka-carousel/pull/436) Refactoring
+
+## 4.4.2 (2018-10-29)
+
+- [#425](https://github.com/FormidableLabs/nuka-carousel/pull/425) Accessibility features added
+
+1. Adding keyboard controls
+2. Reader lets you know what slide you are on
+3. Slide that is being display will be read by reader
+
+- [#435](https://github.com/FormidableLabs/nuka-carousel/pull/435) Fix issues where `clickDisabled` is set to true too often
+- [#433](https://github.com/FormidableLabs/nuka-carousel/pull/433) add function to add ariaProps to all visible slides
+- [#432](https://github.com/FormidableLabs/nuka-carousel/pull/432) Add `slide-visible` class to all currently visible slides
+- [#431](https://github.com/FormidableLabs/nuka-carousel/pull/431) Carousel would go into an infinite loop between two slide. Added a isTransitioning check to wait until afterSlide is finish.
+
+## 4.4.1 (2018-10-08)
+
+- [#423](https://github.com/FormidableLabs/nuka-carousel/pull/423) Prevent click events only when swiping
---
+++ es/default-controls.js
@@ -0,0 +1,236 @@
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+import React from 'react';
+
+var defaultButtonStyles = function defaultButtonStyles(disabled) {
+  return {
+    border: 0,
+    background: 'rgba(0,0,0,0.4)',
+    color: 'white',
+    padding: 10,
+    opacity: disabled ? 0.3 : 1,
+    cursor: disabled ? 'not-allowed' : 'pointer'
+  };
+};
+
+export var PreviousButton =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(PreviousButton, _React$Component);
+
+  function PreviousButton() {
+    var _this;
+
+    _classCallCheck(this, PreviousButton);
+
+    _this = _possibleConstructorReturn(this, (PreviousButton.__proto__ || Object.getPrototypeOf(PreviousButton)).apply(this, arguments));
+    _this.handleClick = _this.handleClick.bind(_assertThisInitialized(_this));
+    return _this;
+  }
+
+  _createClass(PreviousButton, [{
+    key: "handleClick",
+    value: function handleClick(event) {
+      event.preventDefault();
+      this.props.previousSlide();
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var disabled = this.props.currentSlide === 0 && !this.props.wrapAround || this.props.slideCount === 0;
+      return React.createElement("button", {
+        style: defaultButtonStyles(disabled),
+        disabled: disabled,
+        onClick: this.handleClick,
+        "aria-label": "previous"
+      }, "PREV");
+    }
+  }]);
+
+  return PreviousButton;
+}(React.Component);
+export var NextButton =
+/*#__PURE__*/
+function (_React$Component2) {
+  _inherits(NextButton, _React$Component2);
+
+  function NextButton() {
+    var _this2;
+
+    _classCallCheck(this, NextButton);
+
+    _this2 = _possibleConstructorReturn(this, (NextButton.__proto__ || Object.getPrototypeOf(NextButton)).apply(this, arguments));
+    _this2.handleClick = _this2.handleClick.bind(_assertThisInitialized(_this2));
+    _this2.nextButtonDisable = _this2.nextButtonDisabled.bind(_assertThisInitialized(_this2));
+    return _this2;
+  }
+
+  _createClass(NextButton, [{
+    key: "handleClick",
+    value: function handleClick(event) {
+      event.preventDefault();
+      this.props.nextSlide();
+    }
+  }, {
+    key: "nextButtonDisabled",
+    value: function nextButtonDisabled(params) {
+      var wrapAround = params.wrapAround,
+          slidesToShow = params.slidesToShow,
+          currentSlide = params.currentSlide,
+          cellAlign = params.cellAlign,
+          slideCount = params.slideCount;
+      var buttonDisabled = false;
+
+      if (!wrapAround) {
+        var lastSlideIndex = slideCount - 1;
+        var slidesShowing = slidesToShow;
+        var lastSlideOffset = 0;
+
+        switch (cellAlign) {
+          case 'center':
+            slidesShowing = (slidesToShow - 1) * 0.5;
+            lastSlideOffset = Math.floor(slidesToShow * 0.5) - 1;
+            break;
+
+          case 'right':
+            slidesShowing = 1;
+            break;
+        }
+
+        if (slidesToShow > 1) {
+          buttonDisabled = currentSlide + slidesShowing > lastSlideIndex + lastSlideOffset;
+        } else {
+          buttonDisabled = currentSlide + 1 > lastSlideIndex;
+        }
+      }
+
+      return buttonDisabled;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _props = this.props,
+          wrapAround = _props.wrapAround,
+          slidesToShow = _props.slidesToShow,
+          currentSlide = _props.currentSlide,
+          cellAlign = _props.cellAlign,
+          slideCount = _props.slideCount;
+      var disabled = this.nextButtonDisabled({
+        wrapAround: wrapAround,
+        slidesToShow: slidesToShow,
+        currentSlide: currentSlide,
+        cellAlign: cellAlign,
+        slideCount: slideCount
+      });
+      return React.createElement("button", {
+        style: defaultButtonStyles(disabled),
+        disabled: disabled,
+        onClick: this.handleClick,
+        "aria-label": "next"
+      }, "NEXT");
+    }
+  }]);
+
+  return NextButton;
+}(React.Component);
+export var PagingDots =
+/*#__PURE__*/
+function (_React$Component3) {
+  _inherits(PagingDots, _React$Component3);
+
+  function PagingDots() {
+    _classCallCheck(this, PagingDots);
+
+    return _possibleConstructorReturn(this, (PagingDots.__proto__ || Object.getPrototypeOf(PagingDots)).apply(this, arguments));
+  }
+
+  _createClass(PagingDots, [{
+    key: "getDotIndexes",
+    value: function getDotIndexes(slideCount, slidesToScroll, slidesToShow, cellAlign) {
+      var dotIndexes = [];
+      var lastDotIndex = slideCount - slidesToShow;
+
+      switch (cellAlign) {
+        case 'center':
+        case 'right':
+          lastDotIndex += slidesToShow - 1;
+          break;
+      }
+
+      if (lastDotIndex < 0) {
+        return [0];
+      }
+
+      for (var i = 0; i < lastDotIndex; i += slidesToScroll) {
+        dotIndexes.push(i);
+      }
+
+      dotIndexes.push(lastDotIndex);
+      return dotIndexes;
+    }
+  }, {
+    key: "getListStyles",
+    value: function getListStyles() {
+      return {
+        position: 'relative',
+        margin: 0,
+        top: -10,
+        padding: 0
+      };
+    }
+  }, {
+    key: "getListItemStyles",
+    value: function getListItemStyles() {
+      return {
+        listStyleType: 'none',
+        display: 'inline-block'
+      };
+    }
+  }, {
+    key: "getButtonStyles",
+    value: function getButtonStyles(active) {
+      return {
+        border: 0,
+        background: 'transparent',
+        color: 'black',
+        cursor: 'pointer',
+        padding: 10,
+        fontSize: 24,
+        opacity: active ? 1 : 0.5
+      };
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this3 = this;
+
+      var indexes = this.getDotIndexes(this.props.slideCount, this.props.slidesToScroll, this.props.slidesToShow, this.props.cellAlign);
+      return React.createElement("ul", {
+        style: this.getListStyles()
+      }, indexes.map(function (index) {
+        return React.createElement("li", {
+          style: _this3.getListItemStyles(),
+          key: index
+        }, React.createElement("button", {
+          style: _this3.getButtonStyles(_this3.props.currentSlide === index),
+          onClick: _this3.props.goToSlide.bind(null, index),
+          "aria-label": "slide ".concat(index + 1, " bullet")
+        }, "\u2022"));
+      }));
+    }
+  }]);
+
+  return PagingDots;
+}(React.Component);
